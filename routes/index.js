8f8c43304d00c4e967b5cecf62bf0586430286d7
Christopher Lester <chlester@microsoft.com>
2019-09-22 11:27:06 -0700

    setup

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,9 @@
-var  express = require('express');
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-//const fs = require('fs');
 
-// Where fileName is name of the file and response is Node.js Reponse. 
-/*responseFile = (fileName, response) => {
-  const filePath =  "./bea.json" // or any file format
-
-  // Check if file specified by the filePath exists 
-  fs.exists(filePath, function(exists){
-      if (exists) {     
-        // Content-type is very interesting part that guarantee that
-        // Web browser will handle response in an appropriate manner.
-        response.writeHead(200, {
-          "Content-Type": "application/octet-stream",
-          "Content-Disposition": "attachment; filename=" + fileName
-        });
-        fs.createReadStream(filePath).pipe(response);
-      } else {
-        response.writeHead(400, {"Content-Type": "text/plain"});
-        response.end("ERROR File does not exist");
-      }
-    });
-  }
-}
-*/
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 module.exports = router;
