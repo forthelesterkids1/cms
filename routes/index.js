fe7c28ab16330dce9579d87f55a719956ef6e068
Christopher Lester <chlester@microsoft.com>
2019-09-22 12:27:23 -0700

    setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
+var content = fs.readFileSync("bea.json")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: content });
 });
 
 module.exports = router;
